e8f157104bd834359ec2e02a2fce3e94eddec7a2
Mani Kiran <manikiran.nalabolu@gmail.com>
2017-05-30 14:23:08 +0530

    Removing all files in .gitignore

--- server/db/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ORM = require("sequelize");
-var dbUrl = "postgres://postgres:postgres@localhost:5432/complete-typescript-course";
-var options = { benchmark: true, logging: console.log };
-exports.sequelize = new ORM(dbUrl, options);
-//# sourceMappingURL=db.js.map
