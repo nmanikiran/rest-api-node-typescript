afb13bb223f6a21dee62f6e89f77acf463988819
Tushar Gupta <cse.tushar@gmail.com>
2021-04-28 20:05:33 +0530

    Fixed Audit, update dependencies & lint errors (#7)
    * audit fix
    
    * update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import { Application } from "express";
-import * as bodyParser from "body-parser";
-import * as morgan from "morgan";
-import * as fs from "fs";
-import { WriteStream } from "fs";
-import * as path from "path";
-import * as helmet from "helmet";
-import * as winston from "winston";
+import { Application, urlencoded, json } from 'express';
+import * as morgan from 'morgan';
+import * as fs from 'fs';
+import { WriteStream } from 'fs';
+import * as path from 'path';
+import * as helmet from 'helmet';
+import * as winston from 'winston';
 
-import rateLimiter from "./middlewares/rateLimit";
-import { unCoughtErrorHandler } from "./handlers/errorHandler";
-import Routes from "./routes";
+import rateLimiter from './middlewares/rateLimit';
+import { unCoughtErrorHandler } from './handlers/errorHandler';
+import Routes from './routes';
 
 // app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 
@@ -21,19 +20,19 @@ export default class Server {
 
   public config(app: Application): void {
     const accessLogStream: WriteStream = fs.createWriteStream(
-      path.join(__dirname, "./logs/access.log"),
-      { flags: "a" }
+      path.join(__dirname, './logs/access.log'),
+      { flags: 'a' }
     );
-    app.use(morgan("combined", { stream: accessLogStream }));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+    app.use(morgan('combined', { stream: accessLogStream }));
+    app.use(urlencoded({ extended: true }));
+    app.use(json());
     app.use(helmet());
     app.use(rateLimiter()); //  apply to all requests
     app.use(unCoughtErrorHandler);
   }
 }
 
-process.on("beforeExit", function (err) {
+process.on('beforeExit', function (err) {
   winston.error(JSON.stringify(err));
   console.error(err);
 });
