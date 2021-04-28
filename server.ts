b752a12bdd41d0c50ac84bc54c7ff9885a2c255e
nmanikiran <nmanikiran15@gmail.com>
2021-04-28 22:16:23 +0530

    coverted promise to async await

--- server.ts
+++ server.ts
@@ -9,13 +9,13 @@ const app: Application = express();
 const server: Server = new Server(app);
 const port: number = parseInt(process.env.port, 10) || 3000;
 
-app.listen(port, 'localhost', function() {
+app.listen(port, 'localhost', function () {
   console.info(`Server running on : http://localhost:${port}`);
 }).on('error', (err: any) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log('server startup error: address already in use');
-    }
-    else {
-      console.log(err);
-    }
+  if (err.code === 'EADDRINUSE') {
+    console.log('server startup error: address already in use');
+  }
+  else {
+    console.log(err);
+  }
 });
--- src/validators/lessonValidator.ts
+++ src/validators/lessonValidator.ts
@@ -1,22 +1,22 @@
 import * as Joi from 'joi';
 import { Request, Response, NextFunction } from 'express';
 
+interface LessonRequest extends Request {
+  value?: { body?: string };
+}
 export class LessonValidator {
   constructor() { }
 
   validateBody(schema) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        schema.validateAsync(req.body)
-          .then(val => {
-            if (!req['value']) {
-              req['value'] = {};
-            }
-            if (!req['value']['body']) {
-              req['value']['body'] = {};
-            }
-            req['value']['body'] = val;
-            next();
-          }).catch(err => res.status(400).json(err));
+    return async (req: LessonRequest, res: Response, next: NextFunction) => {
+      try {
+        const val = await schema.validateAsync(req.body);
+        req.value = req.value ?? {};
+        req.value.body = req.value.body ?? val;
+        next();
+      } catch (error) {
+        res.status(400).json(error);
+      }
     };
   }
 }
